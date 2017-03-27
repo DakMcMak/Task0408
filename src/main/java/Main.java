76aafc730e62f3a5f487b7f0964563376010a2ef
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-27 23:30:41 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        compare(3);
+        compare(6);
+        compare(5);
+    }
+
+    public static void compare(int a) {
+        if(a < 5){
+            System.out.println("Число меньше 5");
+        }
+        if(a > 5){
+            System.out.println("Число больше 5");
+        }
+        if(a == 5){
+            System.out.println("Число равно 5");
+        }
+    }
+}
